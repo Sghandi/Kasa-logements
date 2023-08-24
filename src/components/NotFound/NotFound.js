bf8b2ebc68ae94d6fb3c1d6aeb26f0d9ec8830b0
Sarah Ghandi <ghandi.sarah33@gmail.com>
2023-08-24 13:37:38 +0200

    First commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Oups ! La page que vous demander n'existe pas</h1>
+        </div>
+    )
+}
